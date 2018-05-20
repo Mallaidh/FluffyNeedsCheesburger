0b3fa2b04b2feb56f55720c0b293fd0ec7c1e6d2
Quinn Painter <quinn@geekymonkey.com>
2018-05-20 12:55:41 +0100

    Dialogue

---
+++ FluffyUnity/Assets/_UI/Scripts/SayStuff.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SayStuff : MonoBehaviour
+{
+    public DialogueInfo dialogueInfo;
+    public float DistanceToShowButton;
+    public GameObject ButtonImage;
+    private FluffyMoveCharacer player;
+    private Animator anim;
+
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+        player = FindObjectOfType<FluffyMoveCharacer>();
+	}
+	
+	void Update ()
+    {
+        ButtonImage.SetActive(Vector3.Distance(player.transform.position, transform.position) < DistanceToShowButton);
+        if (Input.GetButtonDown("ProgressDialogue") && !DialogueManager.instance.IsActive && ButtonImage.activeSelf)
+        {
+            anim.SetBool("Talking", true);
+            player.Animator.SetBool("Talking", true);
+            DialogueManager.instance.StartDialogue(dialogueInfo, this);
+        }
+	}
+
+    public void DialogueEnded()
+    {
+        player.Animator.SetBool("Talking", false);
+        anim.SetBool("Talking", false);
+    }
+}
